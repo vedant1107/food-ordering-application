90665f13254185141a3e08e298fc26986e4a0299
Vedant Deokar <vedantdeokar07@gmail.com>
2025-02-12 22:49:24 +0530

    Database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,20 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => {
+    console.log("db connected");
+    app.listen(7000, () => {
+      console.log("server is running on localhost:7000");
+    });
+  })
+  .catch((error) => {
+    console.log("db connection error: ", error);
+  });
