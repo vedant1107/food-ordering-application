1975608d9f53d5361daf6ab874ca023d291e8784
Vedant Deokar <vedantdeokar07@gmail.com>
2025-02-15 10:56:10 +0530

    Added create user functionality using auth0 login

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/my/user", myUserRoute);
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
