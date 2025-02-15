fe85243c1ddf67f1d4e99c54910e0cc7e357bda3
Vedant Deokar <vedantdeokar07@gmail.com>
2025-02-15 12:46:09 +0530

    Backend | Added update user profile api

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import MyUserController from "../controller/MyUserController";
-import { jwtCheck } from "../middlewares/auth";
+import { jwtCheck, jwtParse } from "../middlewares/auth";
+import { validateMyUserRequest } from "../middlewares/validation";
 
 const router = express.Router();
 
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentUser
+);
 
 export default router;
