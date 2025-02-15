f395497474b8b3fd640292ffa0a7ed6e5e41e0ad
Vedant Deokar <vedantdeokar07@gmail.com>
2025-02-15 22:45:27 +0530

    Backend | Added create restaurant api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
@@ -20,6 +21,7 @@ app.use("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
---
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import multer from "multer";
+import MyRestaurantController from "../controller/MyRestaurantController";
+import { jwtCheck, jwtParse } from "../middlewares/auth";
+import { validateMyRestaurantRequest } from "../middlewares/validation";
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5mb
+  },
+});
+
+router.post(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.createMyRestaurant
+);
+
+export default router;
